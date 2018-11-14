ea5b0cbb683117a76508db92cf20a84375ce1f0c
the_fashionable_coder <khcanniff@users.noreply.github.com>
2018-11-14 08:31:32 -0800

    Added in comments in main for the Extra credit

--- Lab15/main.cpp
+++ Lab15/main.cpp
@@ -12,18 +12,20 @@ using namespace std;
 
 int main(int argc, char *argv[])  //main() must take these parameters when using SDL
 {
-
+	//for the extra credit, we added in 5 sounds (background sound, footsteps, winning sound, ticking sound, pickup sound).
+	// we made the game a collection game. you need to collect the 4 items (1 floating cube, and 3 fruit trees) to win the game.
+	//if you fail to collect all 4 items before the countdown ends, you lose the game.
 	Viewcontroller vc;
 	cout << "Welcome to Adam and Eve Deceives!\n\n"
 		<<"Created by Keely Canniff, Jamie Lynn Culilap, and Naomi Nunis\n\n"
-		<< "A Place where you must find the hidden fruit or you die.\n"
+		<< "A Place where you must find the hidden fruit and the single floating cube hidden amongst other cubes or you die.\n"
 		<< "Need more time find the hidden room that will act as a base and you can finally say you've stopped time.\n\n"
 		<< "Good Luck and Enjoy!\n\n";
 
 	system("pause");
 	vc.run();
 
-	cout << "Game Over! (Place if they won for not)\n\n";
+	cout << "Game Over! (Place if they won or not)\n\n";
 	system("pause");
 	return 0;
 }
