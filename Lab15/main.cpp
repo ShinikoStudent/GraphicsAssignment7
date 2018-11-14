2047bb454b5aa52c67201d7d84bc10ec2ab5fc70
Neunis <n_nunis@u.pacific.edu>
2018-11-13 20:01:21 -0800

    Committed before I added/submitted the files, so heres the actual product

--- Lab15/main.cpp
+++ Lab15/main.cpp
@@ -6,12 +6,21 @@
 //The program ends when the user presses the <Esc> key
 
 #include "viewcontroller.h"
+#include <iostream>
 #include <stdlib.h>
+using namespace std;
 
 int main(int argc, char *argv[])  //main() must take these parameters when using SDL
 {
-	Viewcontroller vc;
 
+	Viewcontroller vc;
+	cout << "Welcome to Adam and Eve Deceives!\n\n"
+		<<"Created by Keely Canniff, Jamie Lynn Culilap, and Naomi Nunis\n\n"
+		<< "A Place where you must find the hidden fruit or you die.\n"
+		<< "Need more time find the hidden room that will act as a base and you can finally say you've stopped time.\n\n"
+		<< "Good Luck and Enjoy!\n\n";
+
+	system("pause");
 	vc.run();
 
 	system("pause");
